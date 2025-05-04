072c74515fc49216b4b15d60b372c7dc53ffbb57
AleksZieba <zeeb3d@gmail.com>
2025-05-04 14:19:46 +0200

    Responsiveness added everything but the modals. Donation button moves to footer on small screens. Made the logo white via css in the NavBar.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,15 +5,29 @@ const Footer: React.FC = () => {
   const year = new Date().getFullYear();
   return (
     <footer className="footer">
+      {/* only shown on small screens */}
+      <a
+        href="https://ko-fi.com/alekszieba"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn-primary footer-donate-button"
+      >
+        Donate ❤️
+      </a>
+
       <div className="footer-left">
         Contact Me:&nbsp;
         <a href="mailto:aleks@alekszieba.com">aleks@alekszieba.com</a>
       </div>
+
       <div className="footer-center">
         © {year} Aleks Zieba
       </div>
+
       <div className="footer-right">
-        <a href="https://www.vecteezy.com/free-vector/symbol"> Symbol Vectors by Vecteezy</a>
+        <a href="https://www.vecteezy.com/free-vector/symbol">
+          Symbol Vectors by Vecteezy
+        </a>
       </div>
     </footer>
   );
